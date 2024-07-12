c85317616490a7a0260fda89dc856829fae7b79f
rafaelmoralesd352 <rafaelmoralesd352@gmail.com>
2024-07-12 23:58:37 +0000

    Rafael Morales

--- lib/validar_contraseña.dart
+++
@@ -1,5 +0,0 @@
-bool _containsUpperCase(String value) {
-  // Expresión regular para verificar que haya al menos una letra mayúscula
-  final RegExp upperCaseRegex = RegExp(r'[A-Z]');
-  return upperCaseRegex.hasMatch(value);
-}
