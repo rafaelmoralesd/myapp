1e36cf69d41173e91e6dcb2dde491036fe1b4ac0
rafaelmoralesd352 <rafaelmoralesd352@gmail.com>
2024-07-12 19:10:19 +0000

    Rafael Morales

--- lib/inicio.dart
+++ lib/inicio.dart
@@ -7,6 +7,7 @@ class   Inicio extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        
         title: const Text('Inicio'),
       ),
     );
